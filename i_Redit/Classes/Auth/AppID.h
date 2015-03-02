79cbbb2bbcfd9892b364e96e980ce26b71898b65
Constantin <botnary@gmail.com>
2015-03-01 22:01:45 -0500

    Test

---
+++ i_Redit/Classes/Auth/AppID.h
@@ -0,0 +1,11 @@
+//
+//  AuthToken.h
+//  i_Redit
+//
+//  Created by Constantin Botnari on 2015-02-25.
+//  Copyright (c) 2015 Constantin Botnari. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT NSString *const REDDIT_APP;
---
+++ i_Redit/Classes/Auth/AppID.m
@@ -0,0 +1,11 @@
+//
+//  AuthToken.m
+//  i_Redit
+//
+//  Created by Constantin Botnari on 2015-02-25.
+//  Copyright (c) 2015 Constantin Botnari. All rights reserved.
+//
+
+#import "AppID.h"
+
+NSString *const REDDIT_APP = @"sCn233gDaZPyAg";
